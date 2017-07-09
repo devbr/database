b7f5b105c7c2fb5a08082dccbf32408bee179d9d
BlackScorpion <prbr@ymail.com>
2017-07-09 01:50:55 -0300

    Inverno

--- User.php
+++ User.php
@@ -9,7 +9,7 @@
  * @copyright 2016 Bill Rocha <http://google.com/+BillRocha>
  * @license   <https://opensource.org/licenses/MIT> MIT
  * @version   GIT: 0.0.1
- * @link      http://paulorocha.tk/devbr
+ * @link      http://dbrasil.tk/devbr
  */
 
 namespace Devbr;
@@ -23,7 +23,7 @@ use Devbr\Database;
  * @package  Util
  * @author   Bill Rocha <prbr@ymail.com>
  * @license  <https://opensource.org/licenses/MIT> MIT
- * @link     http://paulorocha.tk/devbr
+ * @link     http://dbrasil.tk/devbr
  */
 class User
 {
